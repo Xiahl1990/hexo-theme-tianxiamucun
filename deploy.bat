fab48bd1ddb75760eec7f54d6dd5c2ae6ae062c5
Xiahl1990 <971441973@qq.com>
2016-06-27 17:21:51 +0800

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,3 @@
 @echo off
-start cmd /k "hexo version&&hexo clean&&rm -rf .deploy_git db.json&&hexo deploy"
+start cmd /k "hexo version&&hexo clean&&rm -rf .deploy_git db.json&&hexo d"
+start cmd /k "hexo s -p 5000"
